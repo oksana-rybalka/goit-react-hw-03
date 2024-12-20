b3e44734e5dd7b239f6985694a8d405f1da69cfd
oksana-rybalka <rybalka-oksana@outlook.com>
2024-12-20 13:58:39 +0200

    problem with import icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import ContactList from "./ContactList/ContactList";
 import Contact from "./Contact/Contact";
+import style from "./App.module.css";
 
 function App() {
-  const [contact, setContact] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  const [contacts, setContacts] = useState([
+    { id: "id-1", name: "Валерій Залужний", number: "459-12-56" },
+    { id: "id-2", name: "Євген Карась", number: "443-89-12" },
+    { id: "id-3", name: "Конотопська Відьма", number: "645-17-79" },
+    { id: "id-4", name: "Сергій Стерненко", number: "227-91-26" },
   ]);
+
   const deleteContact = (id) => {
     setContacts((prevContacts) =>
       prevContacts.filter((contact) => contact.id !== id)
@@ -17,8 +19,8 @@ function App() {
   };
 
   return (
-    <div>
-      <h1 className={style.titlePhonebook}>Phonebook</h1>
+    <div className={style.parentContainer}>
+      <h1 className={style.titlePhonebook}>Телефонний записник</h1>
       <ContactList contacts={contacts} />
     </div>
   );
--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,18 +1,30 @@
 import style from "./Contact.module.css";
+import { LuPhoneForwarded } from "react-icons/lu";
+import { FaRegUser } from "react-icons/fa";
 
 const Contact = ({ id, name, number, onDelete }) => {
   return (
     <li className={style.contactItem}>
       <div className={style.contactContainer}>
-        <p className={style.contactName}>{name}</p>
-        <p className={style.contactNumber}>{number}</p>
+        <p className={style.contactName}>
+          <span className={style.icon}>
+            <FaRegUser />
+          </span>
+          {name}
+        </p>
+        <p className={style.contactNumber}>
+          <span className={style.icon}>
+            <LuPhoneForwarded />
+          </span>
+          {number}
+        </p>
       </div>
       <button
         className={style.buttonDelete}
         type="button"
         onClick={() => onDelete(id)}
       >
-        Delete
+        Видалити
       </button>
     </li>
   );
